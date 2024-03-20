a0ef8d9528b8047f1e6cd2f32a07dc6df973a49e
Yelyzaveta Pysarenko <162010382+liza-pysarenko@users.noreply.github.com>
2024-03-20 20:55:46 +0200

    Update homework_lesson_8.py

--- homework_lesson_8.py
+++ homework_lesson_8.py
@@ -14,19 +14,7 @@ def convert_salaries(input_file, output_file):
             headers = next(reader)
             data = list(reader)
 
-        for row in data:
-            row[1] = convert_to_uah(float(row[1]))
-            row[2] = convert_to_uah(float(row[2]))
-            row[3] = convert_to_uah(float(row[3]))
-            row[4] = convert_to_uah(float(row[4]))
-            row[5] = convert_to_uah(float(row[5]))
-            row[6] = convert_to_uah(float(row[6]))
-            row[7] = convert_to_uah(float(row[7]))
-            row[8] = convert_to_uah(float(row[8]))
-            row[9] = convert_to_uah(float(row[9]))
-            row[10] = convert_to_uah(float(row[10]))
-            row[11] = convert_to_uah(float(row[11]))
-            row[12] = convert_to_uah(float(row[12]))
+        data = list(map(lambda row: [row[0]] + [convert_to_uah(float(cell)) for cell in row[1:]], data))
 
         with open(output_file, mode='w', newline='') as file:
             writer = csv.writer(file)
